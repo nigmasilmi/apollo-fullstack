720b677552fd79aaba55dd625e0a201b3a95267f
Nigma Silmi <sn.silmi@gmail.com>
2022-07-04 18:46:07 -0400

    connecting data sources with the server

--- README.md
+++ README.md
@@ -4,3 +4,5 @@ https://github.com/r-spacex/SpaceX-API
 
 A data source is any database, service, or API that holds the data you use to populate your schema's fields.
 A data source needs methods that enable it to fetch the data that incoming queries will request.
+
+If you use this.context in a datasource, it's critical to create a new instance of that datasource in the dataSources function, rather than sharing a single instance. Otherwise, initialize might be called during the execution of asynchronous code for a particular user, replacing this.context with the context of another user.
--- server/src/index.js
+++ server/src/index.js
@@ -1,14 +1,26 @@
-require("dotenv").config();
-
 const { ApolloServer } = require("apollo-server");
 const typeDefs = require("./schema");
+const { createStore } = require("./utils");
 
-const server = new ApolloServer({ typeDefs });
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+// set up our SQLite database
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    // datasource with the database as argument
+    userAPI: new UserAPI({ store }),
+  }),
+});
 
 server.listen().then(() => {
   console.log(`
-      Server is running!
-      Listening on port 4000
-      Explore at https://studio.apollographql.com/sandbox
-    `);
+    Server is running!
+    Listening on port 4000
+    Explore at https://studio.apollographql.com/sandbox
+  `);
 });
