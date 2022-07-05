946542b13e25cf1c019b2cf08ac40583d2db2272
Nigma Silmi <sn.silmi@gmail.com>
2022-07-05 10:00:49 -0400

    Querying local fields: login

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,8 +3,10 @@ import {
   NormalizedCacheObject,
   ApolloProvider,
   gql,
+  useQuery,
 } from "@apollo/client";
 import { cache } from "./cache";
+import Login from "./pages/login";
 import React from "react";
 import ReactDOM from "react-dom";
 import Pages from "./pages";
@@ -30,10 +32,21 @@ const client: ApolloClient<NormalizedCacheObject> = new ApolloClient({
 
 injectStyles();
 
+const IS_LOGGED_IN = gql`
+  query IsUserLoggedIn {
+    isLoggedIn @client
+  }
+`;
+
+function IsLoggedIn() {
+  const { data } = useQuery(IS_LOGGED_IN);
+  return data.isLoggedIn ? <Pages /> : <Login />;
+}
+
 // Pass the ApolloClient instance to the ApolloProvider component
 ReactDOM.render(
   <ApolloProvider client={client}>
-    <Pages />
+    <IsLoggedIn />
   </ApolloProvider>,
   document.getElementById("root")
 );
